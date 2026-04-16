7c9adb0d8ad0a596a766e4acd7216d28b4be4a43
JupiterianSoul <gabriel.quart.15@gmail.com>
2026-04-16 22:47:11 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
 
+app = FastAPI(title="Mail to Word")
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
